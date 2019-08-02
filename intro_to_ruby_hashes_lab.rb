f91460bc1df4425d815d01869d137360674f16c7
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-02 16:07:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,7 +50,7 @@ def monopoly_with_fourth_tier
     }, 
     :names => {
       :reading_railroad => {
-        :mortgage_value => "$"
+        :mortgage_value => "$100"
       }, 
       :pennsylvania_railroad => {
         :mortgage_value => "$100"
@@ -59,7 +59,7 @@ def monopoly_with_fourth_tier
         :mortgage_value => "$100"
       }, 
       :shortline_railroad => {
-        :mortgage_value => '$100'
+        :mortgage_value => "$100"
       }
     }
    }

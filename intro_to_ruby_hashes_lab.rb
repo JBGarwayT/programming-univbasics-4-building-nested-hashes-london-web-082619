71d069885a2250353c4fea218d606e115e826945
Jean-Baptiste Garway-Templeman <jbgarwayt@gmail.com>
2019-08-02 15:40:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,10 +1,7 @@
 def base_hash
-  monopoly = {
-    :pieces => 6
-  }
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+	{:pieces => 6}
 end
 
 def monopoly_with_second_tier
